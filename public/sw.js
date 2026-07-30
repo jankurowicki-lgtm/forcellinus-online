6a55aeacc20036f752643e1f1a46fb5ce4647235
jankurowicki-lgtm <jan.kurowicki@gmail.com>
2026-07-30 11:52:12 +0200

    Refresh Greek dataset cache after deployment

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "forcellinus-shell-v3";
+const CACHE = "forcellinus-shell-v4";
 const SHELL = [
   "./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest",
   "./icons/icon.svg", "./art/DP828228-edit.webp", "./art/DP828189-edit.webp",
